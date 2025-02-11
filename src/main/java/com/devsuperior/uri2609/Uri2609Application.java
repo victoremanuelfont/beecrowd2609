1370ab91cf6a18c9ec3dd273efaefcc998da8df6
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-02-11 13:49:01 -0300

    SQL raiz

--- src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2609;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
@@ -19,6 +24,16 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+
+		List<CategorySumProjection> list = repository.search1();
+		List<CategorySumDTO> result1 = list.stream().map(x-> new CategorySumDTO(x)).collect(Collectors.toList());
+
+		System.out.println("\n*** RESULTADO SQL RAIZ: ");
+		for(CategorySumDTO obj : result1){
+			System.out.println(obj);
+		}
+		System.out.println("\n\n");
 
 	}
 }
