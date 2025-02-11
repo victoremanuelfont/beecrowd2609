3bc292904034441374ab5398b1302864e55f3269
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-02-11 14:06:40 -0300

    JPQL

--- src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
+++ src/main/java/com/devsuperior/uri2609/repositories/CategoryRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2609.repositories;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
 import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -19,7 +20,10 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<CategorySumProjection> search1 ();
 
 
-
+    @Query("SELECT new com.devsuperior.uri2609.dto.CategorySumDTO(obj.category.name, SUM(obj.amount)) " +
+            "FROM Product obj " +
+            "GROUP BY obj.category.name ")
+    List<CategorySumDTO> search2 ();
 
 
 
